b30b4f69daafdcf46d10c6a470a9f41ea31d5032
Diana Gabriel <gabrieldiana460@gmail.com>
2026-04-23 12:30:45 -0500

    Update page_object_methods2.py

--- page_object_methods2.py
+++ page_object_methods2.py
@@ -3,28 +3,30 @@ from selenium.webdriver.common.by import By
 
 class RegistrationPageAround:
     # El localizador del campo Correo electrónico
-    email_field = ...
+       email_field = (By.ID, 'email')
     # El localizador del campo Contraseña
-    password_field = ...
+    password_field = (By.ID, 'password')
     # El localizador del botón Registrarse
-    registration_button = ...
+    registration_button = (By.CLASS_NAME, 'auth-form__button')
 
     # El constructor de clase
-    def __init__(self, driver):
-        ...
+  def __init__(self, driver):
+        self.driver = driver
 
     # El método rellena el campo Correo electrónico
     def set_email(self, email):
-        ...
+        self.driver.find_element(*self.email_field).send_keys(email)
 
     # El método rellena el campo Contraseña
     def set_password(self, password):
-        ...
+        self.driver.find_element(*self.password_field).send_keys(password)
 
     # El método hace clic en el botón Registrarse
     def click_registration_button(self):
-        ...
+        self.driver.find_element(*self.registration_button).click()
 
     # El método de registro: combina el correo electrónico, la contraseña y el clic
-    def register(self, ...):
-        ...
+    def register(self, email, password):
+        self.set_email(email)
+        self.set_password(password)
+        self.click_registration_button()
